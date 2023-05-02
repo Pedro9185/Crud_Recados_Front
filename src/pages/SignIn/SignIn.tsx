f4534b2408ba39118da6696ee87394f1407dc79f
Pedro9185 <pedrocatarina.ip@gmail.com>
2023-05-02 09:28:32 -0300

    final-commit

--- src/pages/SignIn/SignIn.tsx
+++ src/pages/SignIn/SignIn.tsx
@@ -8,8 +8,16 @@ import HeaderForm from '../../components/HeaderForm';
 
 const SignIn: React.FC = () => {
   return (
-    <Grid container height="100vh" display="flex" alignItems="center" justifyContent="center" width="100vw">
-      <Grid item xs={12} sm={8} md={5}>
+    <Grid
+      container
+      height="100vh"
+      display="flex"
+      alignItems="center"
+      justifyContent="center"
+      width="100vw"
+      bgcolor="#002884"
+    >
+      <Grid item xs={12} sm={8} md={5} sx={{ borderRadius: '25px', backgroundColor: '#757ce8' }}>
         <Box component="section" marginY={8} marginX={4} display="flex" flexDirection="column" alignItems="center">
           <HeaderForm color={pink[500]} icon={<LockOpenOutlined />} title="Acessar mural de recados" />
           <Form mode="signin" textButton="Entrar" />
--- src/pages/SignUp/SignUp.tsx
+++ src/pages/SignUp/SignUp.tsx
@@ -7,8 +7,16 @@ import HeaderForm from '../../components/HeaderForm';
 
 const SignUp: React.FC = () => {
   return (
-    <Grid container height="100vh" display="flex" alignItems="center" justifyContent="center" width="100vw">
-      <Grid item xs={12} sm={8} md={5}>
+    <Grid
+      container
+      height="100vh"
+      display="flex"
+      alignItems="center"
+      justifyContent="center"
+      width="100vw"
+      bgcolor="#002884"
+    >
+      <Grid item xs={12} sm={8} md={5} sx={{ borderRadius: '25px', backgroundColor: '#757ce8' }}>
         <Box component="section" marginY={8} marginX={4} display="flex" flexDirection="column" alignItems="center">
           <HeaderForm color={green[500]} icon={<VerifiedUser />} title="Cadastre-se" />
           <Form mode="signup" textButton="Criar conta" />
